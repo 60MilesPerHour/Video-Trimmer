36f74e5cd0a4ba26db80af4b9f39e92d49f649b5
Miles Oldenburger <32121785+60MilesPerHour@users.noreply.github.com>
2022-09-26 10:27:13 -0700

    Add files via upload

--- ffmpeg-trimmer.py
+++ ffmpeg-trimmer.py
@@ -2,26 +2,30 @@ import os
 import moviepy
 import moviepy.editor
 
-os.chdir('/home/miles/Desktop/Season3/') # change to the directory you want to trim videos from
+os.chdir(str(input("Enter the directory of the videos: ")))
 
 os.system("ls") # list the files in the directory
-
-num_videos = len(os.listdir())
 
 video_list = []
 for file in os.listdir():
     if file.endswith(".mp4"):
         video_list.append(file)
 
-for i in range(num_videos):
+for i in range(len(os.listdir())):
+
     video_name2 = video_list[i]
     video_name = "'" + video_list[i] + "'"
+
+    print('\n')
+    print("Video Selected: " + video_name2)
+    seconds = int(input("Enter the number of seconds you want to trim from the start: "))
     retrieve_duration = moviepy.editor.VideoFileClip(video_name2)
+
     duration = retrieve_duration.duration
-    # subtract 5 seconds from the duration's end time
+
     duration_end = duration - 10
-    # trim the video
-    os.system("ffmpeg -i " + video_name + " -ss 00:00:05 -to " + str(duration_end) + " -c copy " + video_name.replace(".mp4", "_trimmed.mp4"))
-    # os.system("ffmpeg -i " + video_name + " -ss 00:00:05 -c copy " + video_name.replace(".mp4", "_trimmed.mp4"))
-    print("Video " + str(i+1) + " has been trimmed.")
+
+    os.system("ffmpeg -i " + video_name + " -ss " + str(seconds) + " -to " + str(duration_end) + " -c copy " + video_name.replace(".mp4", "_trimmed.mp4"))
+    print(video_name2 + " has been trimmed.")
+    print("\n")
     print("\n")
